2273f64dad729c2873478cd5c9d7b84e01c5e6b5
ShinikumiAlex <144524709+ShinikumiAlex@users.noreply.github.com>
2024-11-19 18:41:30 +0200

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double x = Func(5) + Func(12) + Func(19);
+            Console.WriteLine(x);
+            Console.ReadKey();
+        }
+        public static double Func(int x)
+        {
+            return Math.Sqrt((x) + x) / 2;
+        }
+    }
+}
